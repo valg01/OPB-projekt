2a6eaa15805c9ebc9d83764ac30bed7d01caf701
tjazerzen <erzen.tjaz@gmail.com>
2023-05-04 17:31:16 +0200

    Add automatic PostgreSQL writing from Python scripts

--- Data/Database.py
+++ Data/Database.py
@@ -3,35 +3,27 @@ import psycopg2, psycopg2.extensions, psycopg2.extras
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s šumniki
 
 from typing import List, TypeVar, Type, Callable
-from Modeli import *
 from pandas import DataFrame
 from re import sub
 import auth_public as auth
 from datetime import date
+import warnings
 
 import dataclasses
 # Ustvarimo generično TypeVar spremenljivko. Dovolimo le naše entitene, ki jih imamo tudi v bazi
 # kot njene vrednosti. Ko dodamo novo entiteno, jo moramo dodati tudi v to spremenljivko.
 
-T = TypeVar(
-    "T",
-    Izdelek,
-    KategorijaIzdelka,
-    CenaIzdelka
-    )
 
 class Repo:
 
+    
     def __init__(self):
         self.conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password, port=5432)
         self.cur = self.conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-
-
+    
+    """
     def dobi_gen(self, typ: Type[T], take=10, skip=0) -> List[T]:
-        """ 
-        Generična metoda, ki za podan vhodni dataclass vrne seznam teh objektov iz baze.
-        Predpostavljamo, da je tabeli ime natanko tako kot je ime posameznemu dataclassu.
-        """
+        # Generična metoda, ki za podan vhodni dataclass vrne seznam teh objektov iz baze. Predpostavljamo, da je tabeli ime natanko tako kot je ime posameznemu dataclassu.
 
 
         # ustvarimo sql select stavek, kjer je ime tabele typ.__name__ oz. ime razreda
@@ -39,11 +31,11 @@ class Repo:
         sql_cmd = f'''SELECT * FROM {tbl_name} LIMIT {take} OFFSET {skip};'''
         self.cur.execute(sql_cmd)
         return [typ.from_dict(d) for d in self.cur.fetchall()]
+    """
     
+    """
     def dobi_gen_id(self, typ: Type[T], id: int, id_col = "id") -> T:
-        """
-        Generična metoda, ki vrne dataclass objekt pridobljen iz baze na podlagi njegovega idja.
-        """
+        # Generična metoda, ki vrne dataclass objekt pridobljen iz baze na podlagi njegovega idja.
         tbl_name = typ.__name__
         sql_cmd = f'SELECT * FROM {tbl_name} WHERE {id_col} = %s';
         self.cur.execute(sql_cmd, (id,))
@@ -54,12 +46,11 @@ class Repo:
             raise Exception(f'Vrstica z id-jem {id} ne obstaja v {tbl_name}');
     
         return typ.from_dict(d)
-    
+    """
+
+    """
     def dodaj_gen(self, typ: T, serial_col="id", auto_commit=True):
-        """
-        Generična metoda, ki v bazo doda entiteto/objekt. V kolikor imamo definiram serial
-        stolpec, objektu to vrednost tudi nastavimo.
-        """
+        # Generična metoda, ki v bazo doda entiteto/objekt. V kolikor imamo definiram serial stolpec, objektu to vrednost tudi nastavimo.
 
         tbl_name = type(typ).__name__
 
@@ -86,12 +77,11 @@ class Repo:
 
         # Dobro se je zavedati, da tukaj sam dataclass dejansko
         # "mutiramo" in ne ustvarimo nove reference. Return tukaj ni niti potreben.
-      
+    """ 
+
+    """
     def dodaj_gen_list(self, typs: List[T], serial_col="id"):
-        """
-        Generična metoda, ki v bazo zapiše seznam objekton/entitet. Uporabi funkcijo
-        dodaj_gen, le da ustvari samo en commit na koncu.
-        """
+        # Generična metoda, ki v bazo zapiše seznam objekton/entitet. Uporabi funkcijo dodaj_gen, le da ustvari samo en commit na koncu.
 
         if len(typs) == 0: return # nič za narest
 
@@ -122,14 +112,11 @@ class Repo:
                 setattr(typs[i], serial_col, d[0])
 
         self.conn.commit()
+    """
 
-
-
+    """
     def posodobi_gen(self, typ: T, id_col = "id", auto_commit=True):
-        """
-        Generična metoda, ki posodobi objekt v bazi. Predpostavljamo, da je ime pripadajoče tabele
-        enako imenu objekta, ter da so atributi objekta direktno vezani na ime stolpcev v tabeli.
-        """
+        # Generična metoda, ki posodobi objekt v bazi. Predpostavljamo, da je ime pripadajoče tabele enako imenu objekta, ter da so atributi objekta direktno vezani na ime stolpcev v tabeli.
 
         tbl_name = type(typ).__name__
         
@@ -151,13 +138,11 @@ class Repo:
         # izvedemo sql
         self.cur.execute(sql_cmd, parameters)
         if auto_commit: self.conn.commit()
-        
+    """    
 
+    """
     def posodobi_list_gen(self, typs : List[T], id_col = "id"):
-        """
-        Generična metoda, ki  posodobi seznam entitet(objektov). Uporabimo isti princip
-        kot pri posodobi_gen funkciji, le da spremembe commitamo samo enkrat na koncu.
-        """
+        # Generična metoda, ki  posodobi seznam entitet(objektov). Uporabimo isti princip kot pri posodobi_gen funkciji, le da spremembe commitamo samo enkrat na koncu.
         
         # Posodobimo vsak element seznama, pri čemer sprememb ne comitamo takoj na bazi
         for typ in typs:
@@ -165,8 +150,9 @@ class Repo:
 
         # Na koncu commitamo vse skupaj
         self.conn.commit()
+    """
 
-
+    
     def camel_case(self, s):
         """
         Pomožna funkcija, ki podan niz spremeni v camel case zapis.
@@ -175,7 +161,7 @@ class Repo:
         s = sub(r"(_|-)+", " ", s).title().replace(" ", "")
         return ''.join(s)     
 
-    def col_to_sql(self, col: str, col_type: str, use_camel_case=True, is_key=False):
+    def col_to_sql(self, col: str, column_types_mapping: str, use_camel_case=True, is_key=False):
         """
         Funkcija ustvari del sql stavka za create table na podlagi njegovega imena
         in (python) tipa. Dodatno ga lahko opremimo še z primary key omejitvijo
@@ -185,27 +171,24 @@ class Repo:
         # ali stolpce pretvorimo v camel case zapis?
         if use_camel_case:
             col = self.camel_case(col)
-        
-        match col_type:
 
+        match column_types_mapping[col]:
             case "int":
-                return f'"{col}" BIGINT{" PRIMARY KEY" if  is_key else ""}'
-            case "int32":
-                return f'"{col}" BIGINT{" PRIMARY KEY" if  is_key else ""}'
-         
-            case "int64":
                 return f'"{col}" BIGINT{" PRIMARY KEY" if  is_key else ""}'
             case "float":
                 return f'"{col}" FLOAT'
-            case "float32":
-                return f'"{col}" FLOAT'
-            case "float64":
-                return f'"{col}" FLOAT'
+            case "bool":
+                return f'"{col}" BOOL'
+            case "date":
+                return f'"{col}" DATE'
+            case "str":
+                return f'"{col}" TEXT{" PRIMARY KEY" if  is_key else ""}'
         
-        # če ni ujemanj stolpec naredimo kar kot text
+        warnings.warn(f'stolpec {col} se ne ujema z nobenim izmed tipov [int, float, bool, date, string]. Shranjujem ta stolpec kot tip TEXT.')
+
         return f'"{col}" TEXT{" PRIMARY KEY" if  is_key else ""}'
     
-    def df_to_sql_create(self, df: DataFrame, name: str, add_serial=False, use_camel_case=True) -> str:
+    def df_to_sql_create(self, df: DataFrame, name: str, column_types_mapping: str, add_serial=False, use_camel_case=True) -> str:
         """
         Funkcija ustvari in izvede sql stavek za create table na podlagi podanega pandas DataFrame-a. 
         df: DataFrame za katerega zgradimo sql stavek
@@ -223,14 +206,13 @@ class Repo:
         
         # dodamo ostale stolpce
         # tukaj bi stolpce lahko še dodatno filtrirali, preimenovali, itd.
-        cols_sql += ",\n".join([self.col_to_sql(col, str(typ), use_camel_case=use_camel_case) for col, typ in cols.items()])
+        cols_sql += ",\n".join([self.col_to_sql(col, column_types_mapping, use_camel_case=use_camel_case) for col, typ in cols.items()])
 
 
         # zgradimo končen sql stavek
         sql = f'''CREATE TABLE IF NOT EXISTS {name}(
             {cols_sql}
         )'''
-
 
         self.cur.execute(sql)
         self.conn.commit()
@@ -263,130 +245,4 @@ class Repo:
         # izvedemo ukaz
         self.cur.execute(sql_cmd)
         self.conn.commit()
-
-
-    def izdelki(self) -> List[IzdelekDto]: 
-        izdelki = self.cur.execute(
-            """
-            SELECT i.id, i.ime, k.oznaka FROM Izdelki i
-            left join KategorijaIzdelka k on i.kategorija = k.id
-            """)
-
-        return [IzdelekDto(id, ime, oznaka) for (id, ime, oznaka) in izdelki]
-    
-    def cena_izdelkov(self) -> List[CenaIzdelkaDto]:
-
-        
-        self.cur.execute(
-            """
-            select c.id, i.id as izdelek_id, i.ime, k.oznaka, c.leto, c.cena from cenaizdelka c
-                left join izdelek i on i.id = c.izdelek_id
-                left join kategorijaizdelka k on k.id = i.kategorija;
-             """
-        )
-
-        return [CenaIzdelkaDto(id, izdelek_id, ime, oznaka, leto, cena) for (id, izdelek_id, ime, oznaka, leto, cena) in self.cur.fetchall()]
-    
-    def dobi_izdelek(self, ime_izdelka: str) -> Izdelek:
-        # Preverimo, če izdelek že obstaja
-        self.cur.execute("""
-            SELECT id, ime, kategorija from Izdelek
-            WHERE ime = %s
-          """, (ime_izdelka,))
-        
-        row = self.cur.fetchone()
-
-        if row:
-            id, ime, kategorija = row
-            return Izdelek(id, ime, kategorija)
-        
-        raise Exception("Izdelek z imenom " + ime_izdelka + " ne obstaja")
-
-    
-    def dodaj_izdelek(self, izdelek: Izdelek) -> Izdelek:
-
-        # Preverimo, če izdelek že obstaja
-        self.cur.execute("""
-            SELECT id, ime, kategorija from Izdelek
-            WHERE ime = %s
-          """, (izdelek.ime,))
-        
-        row = self.cur.fetchone()
-        if row:
-            izdelek.id = row[0]
-            return izdelek
-
-        
-    
-
-        # Sedaj dodamo izdelek
-        self.cur.execute("""
-            INSERT INTO Izdelek (ime, kategorija)
-              VALUES (%s, %s) RETURNING id; """, (izdelek.ime, izdelek.kategorija))
-        izdelek.id = self.cur.fetchone()[0]
-        self.conn.commit()
-        return izdelek
-
-
-    def dodaj_kategorijo(self, kategorija: KategorijaIzdelka) -> KategorijaIzdelka:
-
-
-        # Preverimo, če določena kategorija že obstaja
-        self.cur.execute("""
-            SELECT id from KategorijaIzdelka
-            WHERE oznaka = %s
-          """, (kategorija.oznaka,))
-        
-        row = self.cur.fetchone()
-        
-        if row:
-            kategorija.id = row[0]
-            return kategorija
-
-
-        # Če še ne obstaja jo vnesemo in vrnemo njen id
-        self.cur.execute("""
-            INSERT INTO KategorijaIzdelka (oznaka)
-              VALUES (%s) RETURNING id; """, (kategorija.oznaka,))
-        self.conn.commit()
-        kategorija.id = self.cur.fetchone()[0]
-
-        
-
-        return kategorija
-    
-    def dodaj_ceno_izdelka(self, cena_izdelka: CenaIzdelka) -> CenaIzdelka:
-
-         # Preverimo, če določena kategorija že obstaja
-        self.cur.execute("""
-            SELECT id, izdelek_id, leto, cena from CenaIzdelka
-            WHERE izdelek_id = %s and leto = %s
-          """, (cena_izdelka.izdelek_id, date(int(cena_izdelka.leto), 1, 1)))
-        
-        row = self.cur.fetchone()
-        if row:
-            cena_izdelka.id = row[0]
-            return cena_izdelka
-        
-        # Dodamo novo ceno izdelka
-
-        self.cur.execute("""
-            INSERT INTO CenaIzdelka (izdelek_id, leto, cena)
-              VALUES (%s, %s, %s) RETURNING id; """, (cena_izdelka.izdelek_id, date(int(cena_izdelka.leto), 1, 1), cena_izdelka.cena,))
-        self.conn.commit()
-
-        cena_izdelka.id = self.cur.fetchone()[0]
-        return cena_izdelka
-
-    
-
-    
-
-
-
-
-
-
-
-    
-
+ 
--- Data/uvoz_podatkov.py
+++ Data/uvoz_podatkov.py
@@ -1,37 +1,62 @@
 import pandas as pd
 import numpy as np
-import utils
+from utils import TABLE_DATA
 import auth_public as auth
-import psycopg2
+import requests
+import io
+import warnings
+import Database
 
-DEFAULT_DATE = pd.Timestamp('1900-01-01')
 
-conn = psycopg2.connect(
-    host=auth.host, database=auth.db, user=auth.user, password=auth.password
-)
+DEFAULT_DATE = '1900-01-01'
+USE_CAMEL_CASE = False
+# Če želite tabele odstraniti ter jih napolniti še enkrat, je trenutno treba tabele "na roko" izbrisati ter nastaviti vrednost PONOVNO_ZAPISI_TABELE na True
+PONOVNO_ZAPISI_TABELE = True  # Ne bo ničesar naredilo, če so tabele že ustvarjenje. Znotraj Database.py tabela namreč ustvari preko ukaza "CREATE TABLE IF NOT EXISTS ..."
+PONOVNO_NAPOLNI_TABELE = True  # PAZI: Napolnjevanje tabel ni idempotetna operacija: Če tabelo napolnemo dvakrat, se bodo v naši podatkovni bazi podvojili podatki
+
+
+# Enable the warning filter
+warnings.filterwarnings('always')
 
 
 def ustvari_url(ime_tabele):
     return f"https://raw.githubusercontent.com/jfjelstul/worldcup/master/data-csv/{ime_tabele}.csv"
 
 
+def shrani_df(ime_tabele, column_types_mapping):
+    seznam_imen_stolpcev = column_types_mapping.keys()
+    # Preberi podatke iz Githuba
+    s = requests.get(ustvari_url(ime_tabele)).content
+    df = pd.read_csv(io.StringIO(s.decode('utf-8')), usecols=seznam_imen_stolpcev) 
+    # Nastavitev pravih tipov pandasovih stolpcev
+    for ime_stolpca in seznam_imen_stolpcev:
+        if column_types_mapping[ime_stolpca] == "date":
+            # Nekateri datumi so shranjeni kot NaN, NaT ali "not available". Te nadomestim z default vrednostjo DEFAULT_DATE
+            df[ime_stolpca] = df[ime_stolpca].replace("not available", DEFAULT_DATE)
+            df[ime_stolpca].fillna(DEFAULT_DATE, inplace=True)
+            # not availble zamenjava s tem datumom, ker imava problem v loadu, v SQL bova dala NAN
+            df[ime_stolpca] = pd.to_datetime(df[ime_stolpca], format='%Y-%m-%d')
+        else:
+            df[ime_stolpca] = df[ime_stolpca].astype(column_types_mapping[ime_stolpca])
+    return df
+
+
+repo = Database.Repo()
+
 
 def preberi_in_ustvari(data):
-    for key, dict_columns_and_types in data.items():
-        seznam_imen_stolpcev = dict_columns_and_types.keys()
-        df = pd.read_csv(ustvari_url(key), usecols=seznam_imen_stolpcev)
-        # "CREATE TABLE IF NOT EXISTS "
-        # df.to_sql(key, conn, if_exists="replace", index = False)
-        for ime_stolpca in seznam_imen_stolpcev:
-            if dict_columns_and_types[ime_stolpca] == "date":
-                df[ime_stolpca] = df[ime_stolpca].replace("not available", '1900-01-01')
-                # not availble zamenjava s tem datumom ker imava problem v loadu, v SQL bova dala NAN
-                df[ime_stolpca] = pd.to_datetime(df[ime_stolpca], format='%Y-%m-%d')
-            else:
-                df[ime_stolpca] = df[ime_stolpca].astype(
-                    dict_columns_and_types[ime_stolpca]
-                )
-    conn.close()
+    for ime_tabele, dict_columns_and_types in data.items():
 
+        # Branje in čiščenje/formatiranje tabele
+        df_trenutna = shrani_df(ime_tabele, dict_columns_and_types)
 
-print(preberi_in_ustvari(utils.data))
+        # Ustvarjanje tabele. Bodi pozoren na to, da se znotraj Database.py tabela ustvari preko ukaza "CREATE TABLE IF NOT EXISTS ..."
+        if PONOVNO_ZAPISI_TABELE:
+            repo.df_to_sql_create(df_trenutna, ime_tabele, column_types_mapping=dict_columns_and_types, use_camel_case=USE_CAMEL_CASE)
+
+        # Dodajanje novih podatkov v tabelo
+        if PONOVNO_NAPOLNI_TABELE:
+            repo.df_to_sql_insert(df_trenutna, ime_tabele,use_camel_case=USE_CAMEL_CASE)
+    
+
+preberi_in_ustvari(TABLE_DATA)
