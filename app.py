de89211ed1f5f1c4ae820e64bdeba81fffb60dfc
tjazerzen <erzen.tjaz@gmail.com>
2023-05-11 17:20:39 +0200

    Dodal osnovno funkcionalnost v aplikacijo

--- app.py
+++ app.py
@@ -8,11 +8,10 @@ from Data.Modeli import *
 from Data.Services import AuthService
 from functools import wraps
 import os
-from bottle import TEMPLATE_PATH
 
 SERVER_PORT = os.environ.get('BOTTLE_PORT', 8080)
 RELOADER = os.environ.get('BOTTLE_RELOADER', True)
-TEMPLATE_PATH.insert(0, 'views/')
+
 
 repo = Repo()
 auth = AuthService(repo)
@@ -20,43 +19,48 @@ auth = AuthService(repo)
 def cookie_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        cookie = request.get_cookie("uporabnik")
+        cookie = request.get_cookie("vloga")
+        print('cookie: ', cookie)
         if cookie:
             return f(*args, **kwargs)
-        return template("prijava.html", url=url, napaka="Potrebna je prijava!")
+        return template("vloga.html", url=url, napaka="Za ogled vizualizacij si moraš izbrati vlogo!")
     return decorated
-
-@get('/prijava')
-def prijava():
-    return template('prijava.html', url=url, napaka=None)
-
-
-@post('/prijava')
-def prijava_post():
-    username = request.forms.get('username')
-    password = request.forms.get('password')
-
-    if not auth.obstaja_uporabnik(username):
-        return template("prijava.html", url=url, napaka="Uporabnik s tem imenom ne obstaja")
-
-    prijava = auth.prijavi_uporabnika(username, password)
-    if prijava:
-        response.set_cookie("uporabnik", username)
-        response.set_cookie("rola", prijava.role)
-        redirect('/')  # Redirect to your desired route after successful login
-    else:
-        return template("prijava.html", url=url, napaka="Neuspešna prijava. Napačno geslo ali uporabniško ime.")
-
-@get('/odjava')
-def odjava():
-    response.delete_cookie("uporabnik")
-    response.delete_cookie("rola")
-    return template('prijava.html', url=url, napaka=None)
 
 @get('/')
 @cookie_required
 def index():
-    return template('index.html', url=url)  # Replace with your desired template
+    vloga = request.get_cookie("vloga")
+    return template('predstavitev_vizualizacij.html', url=url, vloga=vloga)
+
+@get('/vloga')
+def prijava_get():
+    return template("vloga.html", url=url, napaka=None)
+
+
+@post('/vloga', name='vloga')
+def prijava():
+    """
+    Prijavi uporabnika v aplikacijo. Vsak uporabnik si lahko izbere eno izmed sledečih vlog: novinec, entuziast, eskpert.
+    Če je prijava vloge uspešna, ustvari piškotke o uporabniku in njegovi roli.
+    Drugače sporoči, da je prijava vloge neuspešna.
+    """
+    vloga = request.forms.get('vloga')
+
+    print('username: ', vloga)
+    if not auth.obstaja_vloga(vloga):
+        return template("vloga.html", napaka="Vloga s tem imenom ne obstaja", url=url)
+
+    response.set_cookie("vloga", vloga)
+    redirect(url('/'))
+
+
+@get('/odjava', name='odjava')
+def odjava():
+    """
+    Odjavi uporabnika iz aplikacije. Pobriše piškotke o uporabniku in njegovi vlogi.
+    """
+    response.delete_cookie("vloga")
+    return template('vloga.html', napaka=None, url=url, vloga=None)
 
 
 if __name__ == "__main__":
