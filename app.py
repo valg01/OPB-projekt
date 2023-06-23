8fe5a813642f1da0f8dbc9ae839f9825948827a7
tjazerzen <erzen.tjaz@gmail.com>
2023-06-23 13:31:28 +0200

    Uredil problem s shranjevanem piškotkov ob registraciji

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ import hashlib
 
 import psycopg2, psycopg2.extensions, psycopg2.extras
 
+from app_utils import RegistracijaUtils
+
+from time import sleep
+
 # problem s šumniki
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
 conn = auth.connect()
@@ -25,58 +29,14 @@ SECRET_COOKIE_KEY = "37d445503ac1ed40e7b45775c0c3cd40"
 
 static_dir = "./static"
 
-"""def include_file(filename):
-    return bottle.static_file(filename, root='./views')
-bottle.SimpleTemplate.defaults["include_file"] = include_file"""
-
-
 repo = Repo()
 auth = AuthService(repo)
-
-# Helper metode
-
-def izracunaj_hash_gesla(geslo):
-    m = hashlib.sha256()
-    m.update(geslo.encode("utf-8"))
-    return m.hexdigest()
-
-def preberi_in_izbrisi_cookie(cookie, default):
-    """Preberi vrednost cookie-ja in ga izbriši"""
-    vrednost = request.get_cookie(cookie, default, secret=SECRET_COOKIE_KEY)
-    response.delete_cookie(cookie, path="/")
-    return vrednost
-
-# TODO: Dodaj tabelo uporabnik v podatkovno bazo
-def preveriUporabnika(): 
-    id_uporabnika = request.get_cookie("id", secret=SECRET_COOKIE_KEY)
-    if id_uporabnika:
-        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        try: 
-            cur.execute("SELECT id_uporabnika FROM uporabnik WHERE id_uporabnika = %s", [id_uporabnika])
-            id_uporabnika = cur.fetchone()[0]
-        except:
-            id_uporabnika = None
-        if id_uporabnika: 
-            return id_uporabnika
-    redirect(url('prijava_get'))
-
-def nastavi_cookie(cookie, vrednost=""):
-    """Nastavi vrednost cookie-ja"""
-    response.set_cookie(cookie, vrednost, path="/", secret=SECRET_COOKIE_KEY)
-
-def nastavi_sporocilo(sporocilo=None):
-    staro = request.get_cookie("sporocilo", secret=SECRET_COOKIE_KEY)
-    if sporocilo is None:
-        response.delete_cookie("sporocilo", path="/")
-    else:
-        response.set_cookie("sporocilo", sporocilo, secret=SECRET_COOKIE_KEY, path="/")
 
 # za ločitev med prijavo in odjavo
 def preveriZnacko():
     if request.get_cookie("id", secret = SECRET_COOKIE_KEY):
         return 1
-    else:
-        return 0
+    return 0
 
 @get("/views/images/<filepath:re:.*\.(jpg|png|gif|ico|svg)>")
 def img(filepath):
@@ -85,25 +45,6 @@ def img(filepath):
 @route("/static/<filename:path>", name="static")
 def static(filename):
     return static_file(filename, root=static_dir)
-
-"""def cookie_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        print("KLIČEM COOKIE_REQUIRED")
-        vloga = request.get_cookie("vloga")
-        ime = request.get_cookie("ime")
-        priimek = request.get_cookie("priimek")
-        email = request.get_cookie("email")
-        geslo1 = request.get_cookie("geslo")
-        geslo2 = request.get_cookie("geslo2")
-
-
-                # emaila tu ne pregledujem, saj ni potreben za prijavo
-
-        if vloga: # and ime and priimek and geslo1 and geslo2:
-            return f(*args, **kwargs)
-        return template("registracija.html", url=url, napaka="Za ogled vizualizacij si moraš izbrati vlogo!", vloga=vloga)
-    return decorated"""
 
 
 # zacetna stran
@@ -114,13 +55,13 @@ def index():
 
 @get('/registracija', name='registracija_get')
 def registracija_get():
-    napaka = nastavi_sporocilo()  # Ker je argument None, bo cookie 'sporocilo' izbrisan
+    napaka = request.get_cookie("sporocilo")
     vloga = request.get_cookie("vloga")
     ime = request.get_cookie("ime")
     priimek = request.get_cookie("priimek")
     email = request.get_cookie("email", "_")
     navijaska_drzava = request.get_cookie("navijaska_drzava")
-    return template("registracija1.html", url=url, napaka=None, vloga=None, ime=ime, priimek=priimek, navijaska_drzava=navijaska_drzava, email=email)
+    return template("registracija1.html", url=url, napaka=napaka, vloga=vloga, ime=ime, priimek=priimek, navijaska_drzava=navijaska_drzava, email=email)
 
 
 @post('/registracija', name='registracija_post')
@@ -133,37 +74,22 @@ def registracija_post():
     geslo2 = request.forms.geslo2 # type: ignore
     navijaska_drzava = request.forms.navijaska_drzava # type: ignore
 
-    try: 
-        cur.execute(f"SELECT * FROM uporabnik WHERE email = {email}")
-        data = cur.fetchall()
-        if data != []:
-            email = None
-    except:
-        email = email
+    registracija_ok_bool, msg = RegistracijaUtils().registracija_ok_q(
+        geslo=geslo, ponovljeno_geslo=geslo2, email=email, cur=cur
+        )
     
-    # print("PRIŠEL DO SEM")
-
-    
-    while True:
-        if email is None:
-            nastavi_sporocilo('Registracija ni možna ta email je že v uporabi!')
-        elif len(geslo) < 4:
-            nastavi_sporocilo('Geslo mora imeti vsaj 4 znake!')
-        elif geslo != geslo2:
-            nastavi_sporocilo('Gesli se ne ujemata!')
-        else: # ni napake, skočimo iz zanke
-            break
+    if not registracija_ok_bool:
+        # nastavi_sporocilo(msg)
+        response.set_cookie("sporocilo", msg) #, secret=SECRET_COOKIE_KEY, path="/")
         # imamo napako, nastavimo piškotke in preusmerimo
-        nastavi_cookie('ime', ime)
-        nastavi_cookie('priimek', priimek)
-        nastavi_cookie('vloga', vloga)
-        nastavi_cookie('navijaska_drzava', navijaska_drzava)
-        nastavi_cookie('email', str(email))
-        
+        response.set_cookie('ime', ime, path="/", secret=SECRET_COOKIE_KEY)
+        response.set_cookie('priimek', priimek, path="/", secret=SECRET_COOKIE_KEY)
+        response.set_cookie('vloga', vloga, path="/", secret=SECRET_COOKIE_KEY)
+        response.set_cookie('navijaska_drzava', navijaska_drzava, path="/", secret=SECRET_COOKIE_KEY)
+        response.set_cookie('email', email, path="/", secret=SECRET_COOKIE_KEY)
         redirect(url('registracija_get'))
     
-    hash_gesla = izracunaj_hash_gesla(geslo)
-    print("Hash gesla:", hash_gesla)
+    hash_gesla = RegistracijaUtils.izracunaj_hash_gesla(geslo)
 
     try:
         print("Vnos v bazo...")
@@ -175,26 +101,12 @@ def registracija_post():
         id_uporabnika = cur.fetchone()[0]
         conn.commit()
         print("Vnos uspešen!")
-        nastavi_cookie('id', id_uporabnika)
+        response.set_cookie('id', id_uporabnika, path="/", secret=SECRET_COOKIE_KEY)
     except:
-        nastavi_sporocilo('Napaka pri vnosu v bazo!')
+        response.set_cookie("sporocilo", "Napaka pri vnosu v bazo!")
     
     redirect(url('registracija_get'))
     return
-    
-
-
-@get('/about', name='about')
-def about():
-    znacka = preveriZnacko()
-    return template("about.html", naslov='O podjetju', znacka=znacka, url=url)
-
-
-@get('/odjava', name='odjava')
-def odjava():
-    response.delete_cookie("id")
-    redirect(url('index'))
-
 
 
 if __name__ == "__main__":
