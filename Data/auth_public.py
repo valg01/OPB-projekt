d970f66b1c8031ecfdd41cf3845602e2d99e1b1a
valg01 <val.groleger@gmail.com>
2023-09-02 13:02:07 +0200

    Posodobljena gesla

--- Data/auth_public.py
+++ Data/auth_public.py
@@ -3,8 +3,8 @@ import psycopg2
 # Skopiraj datoteko v auth.py in vnesi podatke za priklop na bazo
 db = "sem2023_valg"
 host = "baza.fmf.uni-lj.si"
-user = "valg"
-password = "mcw4mi0q"
+user = "javnost"
+password = "javnogeslo"
 
 connection_port = "5432"
 
